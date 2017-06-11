e440790abaad27c1467f834589ebf6c8bf2989bc
Chris Ryan <theramin108@gmail.com>
2017-06-11 11:35:29 -0400

    prepush hook

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Storage from 'react-native-storage';
 import './mqttws31';
 
-export default function initialize(options: object) {
+export default function initialize(options: object): void {
   localStorage = new Storage(options);
 }
 
