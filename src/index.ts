97f74eb0fd6d25ab2e8c258531897b0be28267e1
Chris Ryan <theramin108@gmail.com>
2017-06-11 23:21:20 -0400

    pretty config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import Storage from 'react-native-storage';
-import './mqttws31';
+import Storage from "react-native-storage";
+import "./mqttws31";
 
 export default function init(options: object): void {
   localStorage = new Storage(options);
 }
-
-
-
