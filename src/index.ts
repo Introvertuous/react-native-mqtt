38c7a594ee234dc46ef4f0ecb932b9bf5bef6b7d
Chris <theramin108@gmail.com>
2017-09-06 12:30:02 -0400

    opt for global prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import Storage from "react-native-storage";
-import "./mqttws31";
+import Storage from 'react-native-storage';
+import './mqttws31';
 
 export default function init(options: object): void {
   localStorage = new Storage(options);
