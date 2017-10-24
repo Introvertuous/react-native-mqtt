7c278f586360f5c87807b9c39e568f0595f567b8
Chris <chris@exocortex.com>
2017-10-23 21:49:28 -0400

    comment...

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import Storage from 'react-native-storage';
 import './mqttws31';
 
+/**
+ * see https://github.com/sunnylqm/react-native-storage for more details
+ * @param options {object}
+ */
 export default function init(options: object): void {
   localStorage = new Storage(options);
 }
