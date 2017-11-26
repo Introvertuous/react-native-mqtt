dac5e5c29257a4f9cee8d5f12aba5d70ba6cef3a
Chris <chris@exocortex.com>
2017-11-25 23:17:54 -0500

    testing precommit hook

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import reactNativeStorage from 'react-native-storage';
-import './mqttws31';
+import reactNativeStorage from "react-native-storage";
+import "./mqttws31";
 
 /**
  * see https://github.com/sunnylqm/react-native-storage for more details
  * @param options {object}
  */
+
 export default function init(options: object): void {
   localStorage = new reactNativeStorage(options);
 }
